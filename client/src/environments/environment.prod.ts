822424225cee8afecc8938b0e68e7d953db7e902
Sachi Goto <gotosachiii@gmail.com>
2022-07-27 20:48:37 -0700

    changed http name for production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   name:"Production Env",
-  server:"https://greenaroundtheworld.sachigoto.me"
+  server:"https://ecommerce.server.sachigoto.me"
 
 };
